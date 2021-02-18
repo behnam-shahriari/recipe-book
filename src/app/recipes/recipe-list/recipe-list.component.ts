3a977bb5bfc95a39adf190729315e8885b96c282
Behnam <shahriaribehnam@gmail.com>
2021-02-18 20:48:31 +0200

    Displaying components details in the home page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RecipeModel} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: RecipeModel[] = [
+    new RecipeModel('A test recipe', 'This is simply a test', 'https://i.pinimg.com/originals/8d/c5/31/8dc531fd1168a51c9e5bc9aca0d45051.jpg')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
